810db5a72c95ab9f90847da8b37c65ade12d1604
Intruder <intruder1912@gmail.com>
2015-04-21 20:38:38 +0200

    initial commit... just project setup and added index.xhtml (deleted index.html first), as well as the backing bean for the index page... index page has rudimentary elements that do not work as the backing bean does not provide the referenced actions yet!

---
+++ src/main/java/org/doblander/multitasking/beans/IndexBean.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.doblander.multitasking.beans;
+
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+
+/**
+ * Managed Bean for JSF page index.xhtml.
+ * @author intruder
+ */
+@Named(value = "indexBean")
+@RequestScoped
+public class IndexBean {
+
+    /**
+     * Creates a new instance of IndexBean
+     */
+    public IndexBean() {
+    }
+    
+}
